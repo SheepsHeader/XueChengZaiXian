9e03bbc312861edb6dd223dd26adb6c038a3d900
YH <308723913@qq.com>
2025-06-05 20:16:02 +0800

    day1:构建了content模块的结构

---
+++ xuecheng-plus-base/src/main/java/com/xuecheng/base/model/PageParams.java
@@ -0,0 +1,14 @@
+package com.xuecheng.base.model;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+@Data
+public class PageParams {
+    @ApiModelProperty("当前页码")
+    private Long pageNo = 1L;
+    @ApiModelProperty("每页记录数默认值")
+    private Long pageSize = 30L;
+
+
+}
